4ca1b7b2d3a6d580082958aa4c4d3d22c7c7c174
Shivankp2007 <114836142+Shivankp2007@users.noreply.github.com>
2023-02-22 17:19:47 +0530

    ok

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var body = document.body;
+var darkimg =document.getElementsByClassName('dark-icon');
 
 function togglewb(){
     body.classList.toggle("dark-mode");
-    
-    
 }
 
+function profile(){
+    var x = document.getElementById("profile-dropdown");
+  if (x.style.display == "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
