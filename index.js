cadc0b66239ecd89178684d815d7b5c0739a71e2
Shivankp2007 <114836142+Shivankp2007@users.noreply.github.com>
2023-02-22 20:06:58 +0530

    ok

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
 var body = document.body
-var partwhite = document.getElementsByClassName("part-1")
-var partwhite = document.getElementsByClassName("part-2")
+var partwhite = document.getElementsById("part-2");
 var x = document.getElementById("x");
 
 function Whitemode(){
-  body.classList.toggle('part-1-white')
+  body.classList.toggle('white-mode');
+  partwhite.classList.toggle('part-1-white');
 }
 
 function profile(){
