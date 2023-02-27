bfaf141bc56965491cd46a7430cb09d07f5c10d9
Shivankp2007 <114836142+Shivankp2007@users.noreply.github.com>
2023-02-27 17:44:28 +0530

    ok

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var body = document.body;
 var x = document.getElementById("x");
 var z= document.getElementById("middle");
-var y = document.getElementById('content-setings');
+var y = document.getElementById('set');
 let btn = document.querySelector("#btn");
 let searchBtn = document.querySelector(".bx-search");
-var sidebar = document.getElementById('sidebar');
-var about = document.getElementById('about');
 
+
+//TABS change
 // ICONS change
 var dark2 = document.getElementById('side-bar-icon-dark');
 var white2 = document.getElementById('side-bar-icon-light');
@@ -17,38 +17,40 @@ var white1 = document.getElementById('manage-account-iconwhite-1');
 var backbtn_white = document.getElementById('close-btn-white');
 var backbtn_dark  = document.getElementById('close-btn-dark');
 //END
+function Close_Open() {
+  x.classList.remove('open');
+  z.classList.add('close');
+  y.classList.add('open_content-setings');
+  backbtn_dark.classList.add('open-icon');
+
+}
+
 function Whitemode(){
   body.classList.toggle('white-mode');
   dark.classList.toggle('open-icon');
   dark1.classList.toggle('open-icon');
+  dark2.classList.toggle('open-icon');
   white.classList.toggle('close-icon');
   white1.classList.toggle('close-icon');
-  backbtn_dark.classList.toggle('open-icon');
-  backbtn_white.classList.toggle('close-icon');
-  dark2.classList.toggle('open-icon');
   white2.classList.toggle('close-icon');
+  backbtn_dark.classList.toggle('close-icon');
+  backbtn_white.classList.toggle('open-icon');
+  
+  
 }
 
 function Open_Close(){
-  x.classList.remove('close');
   x.classList.toggle('open');
 }
 
-function Close_Open() {
-  z.classList.add("close");
-  y.classList.add("open-setings");
-  x.classList.remove("open");
-  backbtn_dark.classList.add("close-btn");
-  backbtn_white.classList.add("close-btn");
-  sidebar.classList.add('open-sidebar');
-}
+
 function Close_Back(){
-  z.classList.remove("close");
-  y.classList.remove("open-setings");
-  backbtn_dark.classList.remove("close-btn");
-  backbtn_dark.classList.add("close");
-  backbtn_white.classList.remove("close-btn");
-  backbtn_white.classList.add("close");
-  sidebar.classList.remove('open-sidebar');
+  x.classList.remove('close');
+  z.classList.remove('close');
+  y.classList.remove('open_content-setings');
+  backbtn_white.classList.remove('open-icon');
+  backbtn_dark.classList.remove('open-icon');
+  x.classList.remove('open');
+
 
 }
